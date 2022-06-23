606b7c5065c8b5b55725f1e5d21c0b8ef3556a06
M. Fatih Cırıt <mfc@leodrive.ai>
2022-06-23 15:26:52 +0300

    atomics

--- main.cpp
+++ main.cpp
@@ -1,26 +1,23 @@
+#include <atomic>
 #include <future>
 #include <iostream>
 #include <mutex>
 #include <thread>
 #include <vector>
-#include <atomic>
 
 // prints elements of vector
-void print(const std::vector<int> &v, const std::string &name) {
+void print(const std::vector<std::atomic_int> &v, const std::string &name) {
   std::cout << name << ": ";
-  for (auto i: v)
+  for (const auto &i: v)
     std::cout << i << " ";
   std::cout << std::endl;
 }
 
 int main() {
-
   // vector of numbers 1 to 10000
-  std::vector<int> numbers(10);
+  std::vector<std::atomic<int>> numbers(10);
   for (int i = 0; i < numbers.size(); ++i)
     numbers[i] = i + 1;
-
-  std::vector<std::mutex> numbers_m(numbers.size());
 
   // create a mutex
   std::mutex m_println;
@@ -28,18 +25,14 @@ int main() {
     std::cout << text << std::endl;
   };
 
-  auto make_x = [println_protected, &m_println,
-                 &numbers_m](
-                    std::vector<int> &numbers,
+  auto make_x = [println_protected, &m_println](
+                    std::vector<std::atomic_int> &numbers,
                     int x,
                     size_t ind_start,
                     size_t ind_end,
                     const std::string &name) {
     for (size_t i = ind_start; i <= ind_end; i++) {
-      {
-        std::lock_guard<std::mutex> lock(numbers_m[i]);
-        numbers[i] = x;
-      }
+      numbers[i] = x;
       std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
   };
